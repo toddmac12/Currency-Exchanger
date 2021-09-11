3e1b4fa224ec514c11b9d063c97ac8baef815a52
toddmac12 <toddmac12@gmail.com>
2021-09-11 03:35:25 -0400

    code errors corrected and ran project build

--- src/main.js
+++ src/main.js
@@ -2,89 +2,45 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import currencyExchange from './currency-service.js';
+import Conversion from '../src/currency-service.js';
 
-async function getCall() {
-  let currency1 = document.getElementById("currency1").value;
-  let currency2 = document.getElementById("currency2").value;
-  let amount = document.getElementById("amount").value;
-  try {
-    const response = await CurrencyService.getCurrency(currency1, currency2, amount);
-    if (response.result !== "success") {
-      throw Error(response.statusText);
-    }
-    response.conversion_result === undefined ? document.getElementById("output").innerHTML +=
-      `<li>
-      The current conversion rate from ${currency1} to ${currency2} is ${response.conversion_rate}.
-      Please input an amount in order to perform a conversion.
-      </li>`
-      : document.getElementById("output").innerHTML +=
-      `<li>
-      Converting from ${currency1} to ${currency2} . . . 
-      The conversion rate is ${response.conversion_rate}. ${amount}${currency1} is equal to ${response.conversion_result}${currency2}.
-      </li>`;
-  } catch (error) {
 
-    document.getElementById("output").innerHTML === "Error: Failed to fetch" ? document.getElementById("output").innerHTML += `. <strong>Please input valid currency codes and numerical amount. </strong>`
-      : document.getElementById("output").innerHTML += `<li>Error: Failed to retrieve conversion. Please ensure currencies exist in dropdown list and review syntax.</li>`;
-    return Error(error.message);
-  }
-}
 
-async function getDropDown() {
-  try {
-    const objList = await CurrencyService.getList();
-    const objKeys = Object.keys(objList.conversion_rates);
-    const key = "listKey";
-    sessionStorage.setItem(key, JSON.stringify(objList));
-    for (let i = 0; i < Object.keys(objList.conversion_rates).length; i++) {
-      document.getElementById("currency-list").innerHTML = document.getElementById("currency-list").innerHTML + `<option value="${objKeys[i]}"></option>`;
-      document.getElementById("currency-list2").innerHTML = document.getElementById("currency-list2").innerHTML + `<option value="${objKeys[i]}"></option>`;
-    }
-
-  } catch (error) {
-    document.getElementById("output").innerHTML = Error(error.message) + ". Please verify API keys are correct.";
-    return Error(error.message);
-  }
+function checkInput(dollar) {
+ if (dollar <= 0 || dollar === "") {
+  return new Error("Please enter valid input in both fields!");
+ } else {
+  return true;
+ }
 }
 
 $(document).ready(function () {
-  setTimeout(() => {
-    $("#sub-word").slideDown(1000);
-  }, 3000);
-  setTimeout(() => {
-    $("#third-word").slideDown(1000);
-  }, 4000);
-  setTimeout(() => {
-    $("#new-search").slideDown(1000);
-  }, 5500);
-
-  //Title animation (https://codepen.io/kazed972/pen/bQOQGR)
-  var text = document.getElementById('main-word');
-  var newDom = '';
-  var animationDelay = 95;
-  for (let i = 0; i < text.innerText.length; i++) {
-    newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i]) + '</span>';
-  }
-  text.innerHTML = newDom;
-  var length = text.children.length;
-  for (let i = 0; i < length; i++) {
-    text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
-  }
-
-  $('#convert').click(function () {
-    getCall();
+ $('#convert').click(function () {
+  let dollar = $('#amount').val();
+  let currency = $("#currency").val();
+  let promise = Conversion.convertCurrency();
+  promise.then(function (response) {
+   const body = JSON.parse(response);
+   $('#convertedAmount').html(`The currency rate exchange for $${dollar} USA dollars is ${currency} $${body.conversion_rates[currency] * dollar}`);
+   // $('#displayError').hide();
+  }, function (error) {
+   $('.showErrors').html(`There was an error processing your request: ${error}`);
   });
-
-  document.getElementById("clear-input").onclick = function () {
-    document.getElementById("currency1").value = "";
-    document.getElementById("currency2").value = "";
-    document.getElementById("amount").value = "";
-  };
-
-  document.getElementById("new-search").onclick = function () {
-    $("#main-word, #sub-word, #third-word, #new-search").fadeOut(1000);
-    $("#button-area, #output").slideDown(2000);
-    getDropDown();
-  };
+  try {
+   const isInputValid = checkInput(dollar);
+   if (isInputValid instanceof Error) {
+    console.error(isInputValid.message);
+    throw RangeError("Not a valid entry");
+   } else {
+    console.log("Successful.");
+    $('#convertedAmount').show();
+    $('#displayError').hide();
+   }
+  } catch (error) {
+   console.error(`There's an error! ${error.message}`);
+   // $('#displayError').html
+   $('#displayError').show();
+   $("#convertedAmount").hide();
+  }
+ });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,36 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
 const Dotenv = require('dotenv-webpack');
-
 module.exports = {
-  entry: '../src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
+ entry: './src/main.js',
+ output: {
+  filename: 'bundle.js',
+  path: path.resolve(__dirname, 'dist')
+ },
+ plugins: [
+  new Dotenv(),
+  new CleanWebpackPlugin(), // new line
+  new HtmlWebpackPlugin({
+   title: '',
+   template: 'index.html',
+   inject: 'body',
+  })
+ ],
 
- devtool: 'eval-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
-  plugins: [
-    new CleanWebpackPlugin(), // new line
-    new HtmlWebpackPlugin({
-      title: '',
-      template: 'index.html',
-      inject: 'body'
-    })
-  ],
-
- new Dotenv()
-  ],
-  
- 
-  module: {
-    rules: [
-      {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "eslint-loader"
-      }
+ module: {
+  rules: [
+   {
+    test: /\.css$/,
+    use: [
+     'style-loader',
+     'css-loader'
     ]
-  }
+   },
+   {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'eslint-loader'
+   }
+  ]
+ }
 };
