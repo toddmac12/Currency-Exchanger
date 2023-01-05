252b1758dc5e33d65163044e255b949d871523bf
toddmac12 <toddmac12@users.noreply.github.com>
2023-01-05 00:31:53 -0500

    Update currency-service.js

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,17 +1,17 @@
 export default class Conversion {
- static convertCurrency() {
-  return new Promise(function (resolve, reject) {
-   let request = new XMLHttpRequest();
-   const url =`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-   request.onload = function () {
-    if (this.status === 200) {
-     resolve(request.response);
-    } else {
-     reject(request.response);
-    }
-   };
-   request.open("GET", url, true);
-   request.send();
-  });
- }
+  static convertCurrency() {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      const url =`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
